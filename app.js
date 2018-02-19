da207d4a2c1854acdb221115c797aa3deb76218a
Rafael Gonçalves Lima <rglllima@gmail.com>
2018-02-19 16:48:21 -0300

    testando cards

--- app.js
+++ app.js
@@ -109,7 +109,42 @@ app.post('/webhook/', (req, res) => {
 
 function sendMessage(sender, text_) {
     text_ = text_.substring(0, 319);
-    messageData = { text: text_ };
+//--------------------------------------------------------------------------------------
+// Remover
+    let messageDt = {
+	    "attachment": {
+		    "type": "template",
+		    "payload": {
+				"template_type": "generic",
+			    "elements": [{
+					"title": "First card",
+				    "subtitle": "Element #1 of an hscroll",
+				    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+				    "buttons": [{
+					    "type": "web_url",
+					    "url": "https://www.messenger.com",
+					    "title": "web url"
+				    }, {
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for first element in a generic bubble",
+				    }],
+			    }, {
+				    "title": "Second card",
+				    "subtitle": "Element #2 of an hscroll",
+				    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+				    "buttons": [{
+					    "type": "postback",
+					    "title": "Postback",
+					    "payload": "Payload for second element in a generic bubble",
+				    }],
+			    }]
+		    }
+	    }
+    }
+    messageData = messageDt;
+//---------------------------------------------------------------
+//    messageData = { text: text_ };
 
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
