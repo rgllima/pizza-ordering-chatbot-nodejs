3bd15560299afe30d1c7bbbf4deae5ce066493cd
Rafael Gonçalves Lima <rglllima@gmail.com>
2018-02-21 16:22:14 -0300

    outro teste

--- firebase/fbase.admin.config.js
+++ firebase/fbase.admin.config.js
@@ -9,30 +9,20 @@ const connectToFirebase = () => {
   return admin;
 }
 
-<<<<<<< HEAD
 const salvarPedidos = (admin, respostaWatson, res, sender, payld)=> {
-=======
-const salvarPedidos = (admin, respostaWatson, res, sender, payld, req, res)=> {
->>>>>>> aed4553ca78b14df504bcba534b94c2acd7973d7
   var ret = '';
   var db = admin.database();
   
   if (respostaWatson != null){
      var id = respostaWatson.context.conversation_id;
      db.ref("/pedidos/" + id).set({
-       Data: respostaWatson,
+       Data: respostaWatson.context,
        Status: "Pendente",
        Address: "",
        Name: res.name,
        Id: res.id,
        SenderUser: sender,
-<<<<<<< HEAD
        PayLD: payld
-=======
-       PayLD: payld,
-       Res: res,
-       Req, req
->>>>>>> aed4553ca78b14df504bcba534b94c2acd7973d7
      });
   }
 }
