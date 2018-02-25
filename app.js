a893a468603a07b47f7749b27031ac43386c9e01
Rafael Gonçalves Lima <rglllima@gmail.com>
2018-02-24 21:27:59 -0300

    Adicionando o botão começar - getStarted - teste de necessidade da função

--- app.js
+++ app.js
@@ -271,27 +271,27 @@ function sendTypingOn(sender) {
     });
 }
 /*
- * botão iniciar conversa
- *
+ * Essa função envia uma solicitação POST para registrar o Botão Começar (getStarted)
+ * 
  */
-(function() {
+// (function() {
 
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
-        qs: {
-            access_token: process.env.FB_TOKEN
-        },
-        method: 'POST',
-        json: {
-            "get_started":{
-                "payload":"<GET_STARTED_PAYLOAD>"
-            }
-        }
-    }, function (error, response, body) {
-        if (error) {
-            console.log('Erro no envio da mensagem ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-}())
+//     request({
+//         url: 'https://graph.facebook.com/v2.6/me/messenger_profile',
+//         qs: {
+//             access_token: process.env.FB_TOKEN
+//         },
+//         method: 'POST',
+//         json: {
+//             "get_started":{
+//                 "payload":"<GET_STARTED_PAYLOAD>"
+//             }
+//         }
+//     }, function (error, response, body) {
+//         if (error) {
+//             console.log('Erro no envio da mensagem ', error);
+//         } else if (response.body.error) {
+//             console.log('Error: ', response.body.error);
+//         }
+//     });
+// }())
