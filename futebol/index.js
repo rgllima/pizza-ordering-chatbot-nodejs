d4974af7a4d3ca59236109da8c439c6ec410217f
Rafael Gonçalves Lima <rglllima@gmail.com>
2018-04-17 00:01:15 -0300

    Utilizando o node para fazer a chamada da api de futebol

--- futebol/index.js
+++ futebol/index.js
@@ -118,6 +118,25 @@ app.get('/webview', (req, res, next) => {
         res.sendFile('public/livescore.html', {root: __dirname});
     }
 });
+
+app.get('/getLiveScores', (req, res, next) => {
+
+    request({
+        url: " http://livescore-api.com/api-client/scores/live.json",
+      qs: {
+        key: "6WjGWWrPHEX9YJVq",
+        secret: "mmRh1Fk2aEqxahqgdJX9GoZn3Xw1Grs2"
+      },
+      method: "GET"
+    }, function (error, response, body) {
+        return body;
+        if (error) {
+            console.log('Erro ao buscar dado no servidor', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+});
 //-----------------------------------------------------------------------------------
 app.post('/webhook/', (req, res) => {
     var text = null;
